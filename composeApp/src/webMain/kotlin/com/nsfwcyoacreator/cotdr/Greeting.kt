7b85892a41a18ccafff55e75b279f918f89fbdb7
NSFW CYOA maker <nsfwcyoamaker@gmail.com>
2025-09-30 09:38:37 +0200

    removed template app

--- composeApp/src/webMain/kotlin/com/nsfwcyoacreator/cotdr/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.nsfwcyoacreator.cotdr
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/webMain/kotlin/com/nsfwcyoacreator/cotdr/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.nsfwcyoacreator.cotdr
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
