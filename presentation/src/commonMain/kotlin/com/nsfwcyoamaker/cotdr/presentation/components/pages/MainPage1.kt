bce71d040163828b879463920580e2bc1d81f748
NSFW CYOA maker <nsfwcyoamaker@gmail.com>
2026-01-20 10:05:40 +0100

    Added preview to pages

--- presentation/src/commonMain/kotlin/com/nsfwcyoamaker/cotdr/presentation/components/pages/MainPage1.kt
+++ presentation/src/commonMain/kotlin/com/nsfwcyoamaker/cotdr/presentation/components/pages/MainPage1.kt
@@ -1,13 +1,17 @@
 package com.nsfwcyoamaker.cotdr.presentation.components.pages
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.nsfwcyoamaker.cotdr.presentation.PreviewScope
 import com.nsfwcyoamaker.cotdr.presentation.components.main.MainIntroduction
 import com.nsfwcyoamaker.cotdr.presentation.components.main.MainTitle
 import com.nsfwcyoamaker.cotdr.presentation.components.sections.priestesses_introduction.*
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 fun LazyListScope.MainPage1(
     itemsModifier: Modifier = Modifier,
@@ -41,4 +45,27 @@ fun LazyListScope.MainPage1(
     item { Spacer(modifier = Modifier.height(6.dp)) }
 
     item { PriestessIntroductionExtra2(modifier = itemsModifier) }
+}
+
+@Preview(
+    widthDp = 1920,
+    heightDp = 1080,
+)
+@Composable
+private fun MainPage1Preview() {
+    PreviewScope {
+        val itemsModifier = Modifier
+            .fillMaxWidth(0.95f)
+            .padding(horizontal = 80.dp)
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+        ) {
+            item { Spacer(modifier = Modifier.height(60.dp)) }
+            MainPage1(itemsModifier)
+            item { Spacer(modifier = Modifier.height(260.dp)) }
+        }
+    }
 }
--- presentation/src/commonMain/kotlin/com/nsfwcyoamaker/cotdr/presentation/components/pages/MainPage2.kt
+++ presentation/src/commonMain/kotlin/com/nsfwcyoamaker/cotdr/presentation/components/pages/MainPage2.kt
@@ -1,12 +1,16 @@
 package com.nsfwcyoamaker.cotdr.presentation.components.pages
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.nsfwcyoamaker.cotdr.presentation.PreviewScope
 import com.nsfwcyoamaker.cotdr.presentation.components.sections.fervor.FervorDescription
 import com.nsfwcyoamaker.cotdr.presentation.components.sections.fervor.FervorTitle
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 fun LazyListScope.MainPage2(
     itemsModifier: Modifier = Modifier,
@@ -20,4 +24,27 @@ fun LazyListScope.MainPage2(
     item { Spacer(modifier = Modifier.height(6.dp)) }
 
     // todo
+}
+
+@Preview(
+    widthDp = 1920,
+    heightDp = 1080,
+)
+@Composable
+private fun MainPage1Preview() {
+    PreviewScope {
+        val itemsModifier = Modifier
+            .fillMaxWidth(0.95f)
+            .padding(horizontal = 80.dp)
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+        ) {
+            item { Spacer(modifier = Modifier.height(60.dp)) }
+            MainPage2(itemsModifier)
+            item { Spacer(modifier = Modifier.height(260.dp)) }
+        }
+    }
 }
