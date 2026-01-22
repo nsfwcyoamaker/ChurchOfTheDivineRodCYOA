45b52b2f6fe4d36e1fc3308634b22ce6aae1f07f
NSFW CYOA maker <nsfwcyoamaker@gmail.com>
2026-01-22 15:19:31 +0100

    removed android target from app module

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,20 +1,12 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
 }
 
 kotlin {
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
-
     jvm("desktop")
 
     js {
@@ -47,44 +39,8 @@ kotlin {
             implementation(projects.presentation)
         }
 
-        androidMain.dependencies {
-            implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
-        }
-
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
         }
     }
-}
-
-android {
-    namespace = "com.nsfwcyoamaker.cotdr"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    defaultConfig {
-        applicationId = "com.nsfwcyoamaker.cotdr"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-dependencies {
-    debugImplementation(compose.uiTooling)
 }
