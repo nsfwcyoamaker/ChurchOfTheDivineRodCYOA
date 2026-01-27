a60f5da417bc19be1b9abb9905ae55ca04da653a
NSFW CYOA maker <nsfwcyoamaker@gmail.com>
2026-01-27 15:24:48 +0100

    fix rule

--- domain/src/commonMain/kotlin/com/nsfwcyoamaker/cotdr/domain/rules/BrandsScalingRule.kt
+++ domain/src/commonMain/kotlin/com/nsfwcyoamaker/cotdr/domain/rules/BrandsScalingRule.kt
@@ -10,6 +10,6 @@ object BrandsScalingRule: GlobalScoreRule {
     override fun calculateScore(context: CalculationContext): Resources {
         val count = context.selections.keys.count { it is Brand }
         if (count <= 3) return Resources.Empty //Womb is always free + 2 more, so 3 free
-        return Resources(CotdrCurrency.Fervor, count * -1)
+        return Resources(CotdrCurrency.Fervor, (count - 3) * -1)
     }
 }
